713f153ed17c7ba9746487e81d30b6cd76d5cfb4
Adebisi Tobiloba <101949278+Adebisi1234@users.noreply.github.com>
2023-04-24 14:03:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ mongoose
           "*COMMANDS*: \n\n\n *!ping*: checkout my commands \n\n\n *!join*: send me an invite to your group, \n Format: * *!join* _invite code_* \n\n\n *!anon*: send an anonymous message to specific number, \n Format: * *!anon* _number(with country code)_ _message_* \n\n\n *!group*: send an anonymous message to specific group (I must be in the group), \n Format: * *!group* _group name(not necessarily all, the first word is enough )_ _message_* "
         );
       }if (msg.body === "!chat") {
- msg.reply(await client.getChats().toString())
+const chat = await client.getChats()
+ msg.reply(chat)
 } else if (msg.body.startsWith("!anon ")) {
         // Direct send a new message to specific id
         let number = msg.body.split(" ")[1];
